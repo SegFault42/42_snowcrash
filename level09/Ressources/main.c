d0e3881e91427f2755693c781cabb64f09f86d08
Ramzi BOUGUERRA <rabougue@e1r1p14.42.fr>
2017-09-05 08:30:15 +0200

    level09 ok

---
+++ level09/Ressources/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	int		fd, ret = 0;
+	int		j = 0;
+	char	buff[1024];
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		exit(-1);
+
+	ret = read(fd, &buff, 1024);
+	if (ret <= 0)
+		exit(-1);
+
+	buff[ret] = 0;
+
+	for (int i = 0; i < strlen(buff); ++i)
+	{
+		printf("%c", buff[i] - j);
+		++j;
+	}
+	printf("\n");
+}
